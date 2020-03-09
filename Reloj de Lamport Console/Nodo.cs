85201758b29c1378dadef671d4139210977d3f8a
irasemcai <36616732+irasemcai@users.noreply.github.com>
2020-03-09 00:39:43 -0600

    Reloj Lamport En consola
    Cada proceso tiene su propio contador.
    Un proceso envía un mensaje a otro proceso,su contador  suma 1 y enviará el mensaje junto con su contador.
    El proceso receptor verificará si el contador que viene en el mensaje es mayor a su propio contador, si es así su contador se actualiza al número mayor y suma 1.

---
+++ Reloj de Lamport Console/Nodo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+
+namespace Reloj_de_Lamport_Console
+{
+    class Nodo
+    {
+        public int Id;
+        public int Contador;
+
+        public Nodo (int id)
+        {
+            Id = id;
+            Contador = 0;
+        }
+        public string enviarMensaje(int idReceptor, string mensaje) 
+        {
+            Contador++;
+            Console.WriteLine("***Nodo "+Id+":"+Contador);
+
+            string evento = Id +"->"+ idReceptor + ": "+ Contador;
+            return evento;
+        }
+
+        public string recibirMensaje(int idEmisor, int contadorMensaje, string mensaje)
+        {
+            if (contadorMensaje > Contador)
+            {               
+                Contador = contadorMensaje + 1;                
+            }            
+            else
+            {
+                Contador++;               
+            }
+            Console.WriteLine("***Nodo "+Id +": "+ Contador+ "- "+mensaje);
+
+            string evento = idEmisor + "->" + Id + ": "+Contador;
+            return evento;
+        }        
+    }
+}
---
+++ Reloj de Lamport Console/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Reloj_de_Lamport_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> eventos = new List<string>();
+            List<Nodo> nodos = new List<Nodo>(3);                      
+          
+            Nodo nodo1 = new Nodo(1);
+            Nodo nodo2 = new Nodo(2);
+            Nodo nodo3 = new Nodo(3);
+
+            nodos.Add(nodo1);
+            nodos.Add(nodo2);
+            nodos.Add(nodo3);
+
+            for (int i = 0; i<3; i++)
+            {
+                Console.WriteLine("¿Nodo Emisor? 1, 2 o 3");
+                string emisor = Console.ReadLine();
+                Console.WriteLine("¿Nodo Receptor? 1, 2 o 3");
+                string receptor = Console.ReadLine();
+                Console.WriteLine("Escribe un mensaje: ");
+                string mensaje = Console.ReadLine();
+
+                int EmisorInt= int.Parse(emisor);
+                int ReceptorInt= int.Parse(receptor);
+
+                var nodoEmisor = nodos.Find(n => n.Id == EmisorInt);
+                var nodoRecep = nodos.Find(n => n.Id == ReceptorInt);
+
+                var evento_A = nodoEmisor.enviarMensaje(nodoRecep.Id, mensaje);
+                var evento_B = nodoRecep.recibirMensaje(nodoEmisor.Id, nodoEmisor.Contador, mensaje);
+                eventos.Add(evento_A);
+                eventos.Add(evento_B);
+            }
+            
+            foreach(var evento in eventos)
+            {                
+                Console.WriteLine(evento);
+            }
+
+            
+        }
+    }
+}
